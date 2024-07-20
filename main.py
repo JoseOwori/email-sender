007adf832bd1b144403dcedf9ca9346b418b972d
Jose <114400672+JoseOwori@users.noreply.github.com>
2024-07-20 13:34:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class EmailSchema(BaseModel):
 
 async def send_email(email_data: EmailSchema):
     message = EmailMessage()
-    message["From"] = "info@mpologomacharityfoundation.org"
+    message["From"] = "jmattowori@gmail.com"
     message["To"] = receiver
     message["Subject"] = f"Message from {email_data.name}"
 
